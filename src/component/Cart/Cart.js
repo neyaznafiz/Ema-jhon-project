1c97a294d953f72f34e0df3cc77cb95f0559cb9c
niiyazobug <niiyaz5555@gmail.com>
2022-03-23 14:02:17 +0600

    cart info and total price displayed added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
-    console.log(cart.length)
+const Cart = ({ cart }) => {
+
+    let total = 0
+    for (const product of cart) {
+        total = total + product.price
+    }
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+            <p>Selected Items : {cart.length}</p>
+            <p>Total Price : ${total}</p>
+            <p>Total Shipping : </p>
+            <p>Tax : </p>
+            <h5>Grand Total : </h5>
         </div>
     );
 };
