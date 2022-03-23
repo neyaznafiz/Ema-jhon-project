d2a6d23b4bc2ef0a8ecf0df94f312653a101eac0
niiyazobug <niiyaz5555@gmail.com>
2022-03-23 18:40:52 +0600

    displayed data from local storage

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStroedCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -17,13 +17,27 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    // show previous data from cart in display
+    useEffect(() => {
+        const storedCart = getStroedCart()
+        const savedCart = []
+        for (const id in storedCart) {
+            const addedProducts = products.find(product => product.id === id)
+            if (addedProducts) {
+                const quantity = storedCart[id]
+                addedProducts.quantity = quantity
+                savedCart.push(addedProducts)
+            }
+        }
+        setCart(savedCart)
+    }, [products])
+
     const handleAddToCart = (product) => {
         //do not do this: cart.push(product)
-        const newCart= [...cart, product]
+        const newCart = [...cart, product]
         setCart(newCart)
         addToDb(product.id)
     }
-
     return (
         <div className='shop-container'>
             <div className="products-container">
