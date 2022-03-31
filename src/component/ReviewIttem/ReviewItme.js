7faaf4b1cdb41bbcad3c63d2ca6d95abd16202e0
niiyazobug <niiyaz5555@gmail.com>
2022-03-31 14:07:05 +0600

    slice and title adde in name and css added in review component

--- src/component/ReviewIttem/ReviewItme.js
+++ src/component/ReviewIttem/ReviewItme.js
@@ -14,7 +14,9 @@ const ReviewItme = (props) => {
 
                 <div className="review-item-details">
 
-                    <p className="product-name">{name}</p>
+                    <p className="product-name" title={name}>
+                        {name.length > 20 ? name.slice(0, 20) + '...' : name}
+                    </p>
                     <p>Price: <span className='orange-color'> ${price}</span></p>
                     <p><small> Shipping: {shipping}</small></p>
                     <p><small>Quantity: {quantity}</small></p>
@@ -22,7 +24,7 @@ const ReviewItme = (props) => {
                 </div>
 
                 <div className="delete-container">
-
+                    <button>Delete</button>
                 </div>
             </div>
         </div>
