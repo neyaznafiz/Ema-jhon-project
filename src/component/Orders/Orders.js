6be9fc1d6a2131a75d4acf8f81539fc1a6082db1
niiyazobug <niiyaz5555@gmail.com>
2022-03-31 12:04:15 +0600

    cart component used in order component

--- src/component/Orders/Orders.js
+++ src/component/Orders/Orders.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
 import useCart from '../Hooks/useCart';
 import useProducts from '../Hooks/useProducts';
+import '../Shop/Shop.css'
 
 const Orders = () => {
 
     const [products, setProducts] = useProducts()
 
-    const [cart, setCart]= useCart(products)
+    const [cart, setCart] = useCart(products)
 
     return (
-        <div>
-            <h2>This is Order: {products.length}</h2>
-            <p>Cart has: {cart.length}</p>
+        <div className='shop-container'>
+            {/* <h2>This is Order: {products.length}</h2>
+            <p>Cart has: {cart.length}</p> */}
+            <div className="products-container">
+
+            </div>
+
+            <div className="cart-container">
+
+                <Cart cart={cart}></Cart>
+
+            </div>
+
         </div>
     );
 };
