3781d8e384a0b854dcf53d196bc6935d08692682
niiyazobug <niiyaz5555@gmail.com>
2022-04-10 02:02:20 +0600

    handle blur added in signup input

--- src/component/Signup/Signup.js
+++ src/component/Signup/Signup.js
@@ -1,50 +1,69 @@
 import React from 'react';
+import { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 import './Signup.css'
 
 const Signup = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confiemPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value)
+    }
+
     return (
         <div className='form-container'>
-        <div>
-            <h2 className='form-title'>Sign Up</h2>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
 
-            <form action="">
+                <form action="">
 
-                <div className="input-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="" required />
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+
+                    <input className='form-submit' type="submit" value="Sign Up" />
+
+                </form>
+                <p>
+                    Already have an accout? <Link to='/login' className='form-link'>Login</Link>
+                </p>
+
+                <div className='or'>
+                    <div className='hr'> <hr /> </div>
+                    <div className='or-div'> or </div>
+                    <div className='hr'> <hr /> </div>
                 </div>
 
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="" required />
+                <div className='google'>
+                    <button className='google-button'><FcGoogle className='google-logo'></FcGoogle> Continue with google</button>
                 </div>
 
-                <div className="input-group">
-                    <label htmlFor="confirm-password">Confirm Password</label>
-                    <input type="password" name="confirm-password" id="" required />
-                </div>
-
-                <input className='form-submit' type="submit" value="Sign Up" />
-
-            </form>
-            <p>
-                Already have an accout? <Link to='/login' className='form-link'>Login</Link>
-            </p>
-
-            <div className='or'>
-                <div className='hr'> <hr /> </div>
-                <div className='or-div'> or </div>
-                <div className='hr'> <hr /> </div>
             </div>
-
-            <div className='google'>
-            <button className='google-button'><FcGoogle className='google-logo'></FcGoogle> Continue with google</button>
-            </div>
-
         </div>
-    </div>
     );
 };
 
