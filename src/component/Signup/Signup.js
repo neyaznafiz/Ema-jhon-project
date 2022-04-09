ddfecc5c7f1852c6ecf794d8c5e917e8343bf50e
niiyazobug <niiyaz5555@gmail.com>
2022-04-10 01:51:15 +0600

    required added in input feield

--- src/component/Signup/Signup.js
+++ src/component/Signup/Signup.js
@@ -13,17 +13,17 @@ const Signup = () => {
 
                 <div className="input-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="" />
+                    <input type="email" name="email" id="" required />
                 </div>
 
                 <div className="input-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="" />
+                    <input type="password" name="password" id="" required />
                 </div>
 
                 <div className="input-group">
                     <label htmlFor="confirm-password">Confirm Password</label>
-                    <input type="password" name="confirm-password" id="" />
+                    <input type="password" name="confirm-password" id="" required />
                 </div>
 
                 <input className='form-submit' type="submit" value="Sign Up" />
