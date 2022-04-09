c82bb8c3a6e3f6b05ed27586d173227bbd36175b
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 23:05:24 +0600

    install firebase and init firebase

---
+++ src/Firebase/firebase,init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhiCTFmstC_WBAa2rfYHxLqtepGK3WJPE",
+  authDomain: "ema-jhon-project-f1f64.firebaseapp.com",
+  projectId: "ema-jhon-project-f1f64",
+  storageBucket: "ema-jhon-project-f1f64.appspot.com",
+  messagingSenderId: "43023464900",
+  appId: "1:43023464900:web:be4a34d276fc09d6c38c91"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+
+export default app
---
+++ src/__auth_steps.js
@@ -0,0 +1,7 @@
+/**
+ * --------------- 10-4-2022-----------------
+ * 1. create a new project firebase.google.com
+ * 2. npm install firebase
+ * 3.
+ */
+ 
