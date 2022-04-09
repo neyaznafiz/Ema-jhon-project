118ce0693c1999c306aa3f1fa81eae856ff67f37
niiyazobug <niiyaz5555@gmail.com>
2022-04-09 23:10:53 +0600

    hosting note

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -2,6 +2,17 @@
  * --------------- 10-4-2022-----------------
  * 1. create a new project firebase.google.com
  * 2. npm install firebase
- * 3.
+ * 3. create firebase.init and import getAuth to export
+ * 4. 
  */
  
+
+
+
+/**
+ * hosting steps
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 3. firebase init
+ * 4. firebase deploy
+*/
