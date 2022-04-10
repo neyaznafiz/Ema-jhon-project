11fa50fc1ac1f3ceb7bc0fb19194470f526d54f0
niiyazobug <niiyaz5555@gmail.com>
2022-04-10 20:23:06 +0600

    use reqireauth for inventory page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './component/Header/Header';
 import Inventory from './component/Inventory/Inventory';
 import Login from './component/Login/Login';
 import Orders from './component/Orders/Orders';
+import RequireAuth from './component/RequireAuth/RequireAuth';
 import Shop from './component/Shop/Shop';
 import Signup from './component/Signup/Signup';
 
@@ -16,12 +17,19 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+
+        {/* its a ruquire page, you havo to be login to visit this page  */}
+        <Route path='/inventory' element={
+          <RequireAuth>
+            <Inventory></Inventory>
+          </RequireAuth>
+        }></Route>
+
         <Route path='/about' element={<Abouts></Abouts>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
       </Routes>
-      
+
       {/* <Shop></Shop> */}
     </div>
   );
