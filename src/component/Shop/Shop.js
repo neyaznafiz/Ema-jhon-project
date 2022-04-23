e5a021732cb522099edd6f75b748c2959a8eb169
niiyazobug <niiyaz5555@gmail.com>
2022-04-24 03:03:51 +0600

    database link in useProducts and in changed into mongo id in shop.js

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -58,7 +58,7 @@ const Shop = () => {
             <div className="products-container">
                 {
                     products.map(product => <Product
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                     ></Product>)
