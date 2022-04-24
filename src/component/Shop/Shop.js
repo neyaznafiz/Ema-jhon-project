a153f255a84e6e06e369c47aa267ebe0947fe695
niiyazobug <niiyaz5555@gmail.com>
2022-04-25 00:07:27 +0600

    useProduct hook methodd copy and pase it shop directly and some page selected added

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -11,12 +11,24 @@ import './Shop.css'
 const Shop = () => {
     // const [products, setProducts] = useState([])
 
-    const [products, setProducts] = useProducts()
+    // const [products, setProducts] = useProducts()
 
     const [cart, setCart] = useState([])
 
     const [pageCount, setPageCount] = useState(0)
     const [page, setPage] = useState(0)
+    const [size, setSize]=useState(10)
+
+    // copy from useProducts
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/product')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+            
+    }, [])
 
     useEffect(() => {
 
@@ -86,6 +98,12 @@ const Shop = () => {
                         className={page === number ? 'selected': ''}
                         >{number + 1}</button>)
                     }
+                    <select onChange={(e)=>setSize(e.target.value)} name="" id="">
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
                 </div>
             </div>
 
